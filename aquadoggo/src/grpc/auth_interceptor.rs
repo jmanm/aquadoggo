3d6c6dd52df543530b42a4be65aeba6e6db6e710
Jamin M <jaminmenter@outlook.com>
2025-01-04 11:35:57 -0700

    implement JWS verification of payload

--- aquadoggo/src/grpc/auth_interceptor.rs
+++ aquadoggo/src/grpc/auth_interceptor.rs
@@ -1,23 +1,62 @@
+use ed25519_dalek::pkcs8::EncodePublicKey;
+use ed25519_dalek::{VerifyingKey, PUBLIC_KEY_LENGTH};
+use josekit::{jws::EdDSA, jwt};
+use log::{debug, warn};
 use tonic::{service::Interceptor, Request, Status};
 
 #[derive(Clone)]
-pub struct AuthInterceptor {
-    // Add any fields you need
+pub struct AuthInterceptor {}
+
+impl AuthInterceptor {
+    fn validate_token(pub_key: &str, token: &str) -> bool {
+        debug!("Received pub key {} and token {}", pub_key, token);
+        if let Ok(key_bytes) = hex::decode(pub_key) {
+            if key_bytes.len() == PUBLIC_KEY_LENGTH {
+                let verifying_key_bytes: [u8; PUBLIC_KEY_LENGTH] = key_bytes.try_into().unwrap();
+                if let Ok(verifiying_key) = VerifyingKey::from_bytes(&verifying_key_bytes) {
+                    if let Ok(der) = verifiying_key.to_public_key_der() {
+                        if let Ok(verifier) = EdDSA.verifier_from_der(der) {
+                            if let Ok((payload, _header)) =
+                                jwt::decode_with_verifier(token, &verifier)
+                            {
+                                debug!("Payload: {}", &payload);
+                                return true;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        warn!(
+            "Invalid auth headers; pub key: {}, token: {}",
+            pub_key, token
+        );
+        false
+    }
 }
 
 impl Interceptor for AuthInterceptor {
     fn call(&mut self, request: Request<()>) -> Result<Request<()>, Status> {
-        // Get metadata from request
         let metadata = request.metadata();
+        let key_header = metadata.get("pubKey");
+        let token_header = metadata.get("token");
 
-        // Example: Check for authorization header
-        if let Some(auth_header) = metadata.get("authorization") {
-            if !auth_header.is_empty() {
-                // if validate_token(auth_header.to_str().unwrap()) {
-                return Ok(request);
-                // }
+        match (key_header, token_header) {
+            (Some(key_cookie), Some(token_cookie)) => {
+                let key_str = key_cookie.to_str();
+                let token_str = token_cookie.to_str();
+                match (key_str, token_str) {
+                    (Ok(pub_key), Ok(token)) => {
+                        if Self::validate_token(pub_key, token) {
+                            Ok(request)
+                        } else {
+                            Err(Status::unauthenticated("Incorrect signature"))
+                        }
+                    }
+                    _ => Err(Status::unauthenticated("Invalid header values")),
+                }
             }
+            _ => Err(Status::unauthenticated("Missing required headers")),
         }
-        Err(Status::unauthenticated("No authorization header"))
     }
 }
